dbd130f5cdebac495db01598c299b866bb44577a
Carlos <bwaycarl@gmail.com>
2020-07-16 16:21:59 -0400

    Building HTML & JS

---
+++ script.js
@@ -0,0 +1,10 @@
+const start = document.getElementById("start")
+const quiz = document.getElementById("quiz")
+const queston = document.getElementById("question")
+const timer = document.getElementById("timer")
+
+const choiceA = document.getElementById("A")
+const choiceB = document.getElementById("B")
+const ChoiceC = document.getElementById("C")
+const ChoiceD = document.getElementById("D")
+
