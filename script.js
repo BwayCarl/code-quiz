9b1ccad8f53606fc99e3f1410e88e6faba2a3896
Carlos <bwaycarl@gmail.com>
2020-07-16 17:17:26 -0400

    Add Media Query to CSS, add JS.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Elements
 const start = document.getElementById("start")
 const quiz = document.getElementById("quiz")
 const queston = document.getElementById("question")
@@ -5,6 +6,25 @@ const timer = document.getElementById("timer")
 
 const choiceA = document.getElementById("A")
 const choiceB = document.getElementById("B")
-const ChoiceC = document.getElementById("C")
-const ChoiceD = document.getElementById("D")
+const choiceC = document.getElementById("C")
+const choiceD = document.getElementById("D")
 
+// Rendering the questions
+
+let lastQuestionIndex = questions.length -1;
+let runningQuestionIndex = 0;
+
+function renderQuestion() {
+    let q =questions[runningQuestionIndex];
+    question.innerHTML - "<p>" + q.question + "</p>";
+        choiceA.innerHTML = q.choiceA;
+        choiceB.innerHTML = q.choiceB;
+        choiceC.innerHTML = q.choiceC;
+        choiceD.innerHTML = q.choiceD;
+}
+
+//Next question
+
+function renderQuestion() {
+    runningQuestionIndex++;
+}
